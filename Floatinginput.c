4bd07336d4ab0fa2bd52ff8be1bb3bcb0230b70c
Irshath Vasur <98567387+Irshathvasur@users.noreply.github.com>
2022-09-18 19:48:04 +0530

    Assignment for c 1

---
+++ Floatinginput.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    float num1;
+    double num2;
+    printf("Enter two floating points ");
+    scanf("%f %lf",&num1,&num2);
+    printf("Number1=%f\n",num1);
+    printf("Number2=%lf",num2);
+    return 0;
+}
---
+++ asciiinput.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    char ch;
+    printf("Enter a character ");
+    scanf("%c",&ch);
+    //printing char as character
+    printf("Character=%c\n",ch);
+    //printing ASCII value
+    printf("ASCII value of this charcter=%d",ch);
+    return 0; 
+}
