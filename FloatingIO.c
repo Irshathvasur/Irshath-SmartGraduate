7c99fbfd09c38e815fb0b746407a3c8b33f81c58
Irshath Vasur <98567387+Irshathvasur@users.noreply.github.com>
2022-09-18 19:31:57 +0530

    Update FloatingIO.c

--- FloatingIO.c
+++ FloatingIO.c
@@ -3,7 +3,7 @@
 int main(){
     float num1=2.34;
     double num2=234.332;
-    printf("Number1=%f",num1);
-    printf("\nNumber2=%lf",num2);
+    printf("Number1=%0.2f",num1);
+    printf("\nNumber2=%0.3lf",num2);
     return 0;
 }
