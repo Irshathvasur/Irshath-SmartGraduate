ae4072a29f2687c6b5fede14b121e48531ad04f9
Irshath Vasur <98567387+Irshathvasur@users.noreply.github.com>
2022-09-18 19:25:47 +0530

    Assignment for c 1

---
+++ CharacterIO.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    char ch='j';
+    printf("Character=%c",ch);
+    return 0;
+}
---
+++ FloatingIO.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    float num1=2.34;
+    double num2=234.332;
+    printf("Number1=%f",num1);
+    printf("\nNumber2=%lf",num2);
+    return 0;
+}
