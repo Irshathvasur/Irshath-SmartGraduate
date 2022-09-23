336fdf5f58c098cb7e834767fc7eaaab35d2a107
Irshath Vasur <98567387+Irshathvasur@users.noreply.github.com>
2022-09-23 18:32:31 +0530

    Assignment for java 1

---
+++ Sum.java
@@ -0,0 +1,11 @@
+package Practicecodes.Java;
+
+public class Sum{
+        public static void main(String[] args) {
+            int first=50;
+            int second=150;
+            System.out.println(first+" "+second);
+            int sum=first+second;
+            System.out.println("The sum is: "+sum);
+    }
+}
